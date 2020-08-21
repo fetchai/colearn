f3deb9a50c11da510292b59bc8d0ce55639221dd
Juan Besa <juan.besa@fetch.ai>
2020-08-21 17:45:28 +0100

    Should pass tests

--- colearn_interface/test_standalone_driver.py
+++ colearn_interface/test_standalone_driver.py
@@ -2,7 +2,7 @@ from colearn_interface.standalone_driver import add
 
 
 def test_sum():
-    assert add(1,2) == 5
+    assert add(1,2) == 3
 
 if __name__ == "__main__":
     # execute only if run as a script
