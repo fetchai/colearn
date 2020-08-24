4c11bf37691889992f65be16f5abaaad83d33732
Juan Besa <juan.besa@fetch.ai>
2020-08-24 17:36:36 +0100

    Added a test

---
+++ colearn_interface/test_plus_one_learner.py
@@ -0,0 +1,8 @@
+from colearn_interface.plus_one_learner import PlusOneLearner
+
+
+def test_init():
+    learner = PlusOneLearner(0)
+    assert learner.current_value == 0
+    learner_2 = PlusOneLearner(2)
+    assert learner_2.current_value == 2
