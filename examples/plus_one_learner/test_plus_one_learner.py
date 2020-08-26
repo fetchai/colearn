bd15d956cc64d1eb6fecf33a9d9f9943f44c40af
Juan Besa <juan.besa@fetch.ai>
2020-08-26 13:12:03 +0100

    Fixed the test

--- examples/plus_one_learner/test_plus_one_learner.py
+++ examples/plus_one_learner/test_plus_one_learner.py
@@ -5,4 +5,4 @@ def test_init():
     learner = PlusOneLearner(0)
     assert learner.current_value == 0
     learner_2 = PlusOneLearner(2)
-    assert learner_2.current_value == 4
+    assert learner_2.current_value == 2
