29362aec29ff87da50e89d5ddd9d85dfbba2d26d
Emma Smith <emma.smith@fetch.ai>
2020-09-03 15:50:40 +0100

    Refactor DataConfig

--- examples/fraud/config.py
+++ examples/fraud/config.py
@@ -7,34 +7,28 @@ from .models import FraudSVMLearner
 tf.disable_v2_behavior()
 
 
-def load_config(config):
-    # Training params
-    config.batch_size = 10000
-
-    # Model params
-    config.model_type = FraudSVMLearner
-
-    config.input_classes = 431
-    config.loss = "binary_crossentropy"
-    config.n_classes = 1
-    config.multi_hot = False
-
-    # Keras only params
-    config.loss = "sparse_categorical_crossentropy"
-    config.optimizer = tf.keras.optimizers.Adam
-    config.l_rate = 0.001
-    config.l_rate_decay = 1e-5
-
-    # Data params
-    config.steps_per_epoch = 1
-
-    config.train_ratio = 0.8
-    config.val_batches = 1  # number of batches used for voting
-    config.test_ratio = 1 - config.train_ratio
-
-
 class FraudConfig(ModelConfig):
     def __init__(self):
         super().__init__()
+
+        # Training params
+        self.batch_size = 10000
+
+        # Model params
+        self.model_type = FraudSVMLearner
         self.input_classes = 431
-        load_config(self)
+        self.loss = "binary_crossentropy"
+        self.n_classes = 1
+        self.multi_hot = False
+
+        # Keras only params
+        self.loss = "sparse_categorical_crossentropy"
+        self.optimizer = tf.keras.optimizers.Adam
+        self.l_rate = 0.001
+        self.l_rate_decay = 1e-5
+
+        # Data params
+        self.steps_per_epoch = 1
+        self.train_ratio = 0.8
+        self.val_batches = 1  # number of batches used for voting
+        self.test_ratio = 1 - self.train_ratio
--- examples/mnist/config.py
+++ examples/mnist/config.py
@@ -7,32 +7,26 @@ from colearn.config import ModelConfig
 tf.disable_v2_behavior()
 
 
-def load_config(config):
-
-    # Training params
-    config.optimizer = tf.keras.optimizers.Adam
-    config.l_rate = 0.001
-    config.l_rate_decay = 1e-5
-    config.batch_size = 64
-
-    # Model params
-    config.model_type = MNISTSuperminiLearner
-
-    config.width = 28
-    config.height = 28
-    config.loss = "sparse_categorical_crossentropy"
-    config.n_classes = 10
-    config.multi_hot = False
-
-    # Data params
-    config.steps_per_epoch = None
-
-    config.train_ratio = 0.8
-    config.val_batches = 2  # number of batches used for voting
-    config.test_ratio = 1 - config.train_ratio
-
-
 class MNISTConfig(ModelConfig):
     def __init__(self):
         super().__init__()
-        load_config(self)
+
+        # Training params
+        self.optimizer = tf.keras.optimizers.Adam
+        self.l_rate = 0.001
+        self.l_rate_decay = 1e-5
+        self.batch_size = 64
+
+        # Model params
+        self.model_type = MNISTSuperminiLearner
+        self.width = 28
+        self.height = 28
+        self.loss = "sparse_categorical_crossentropy"
+        self.n_classes = 10
+        self.multi_hot = False
+
+        # Data params
+        self.steps_per_epoch = None
+        self.train_ratio = 0.8
+        self.val_batches = 2  # number of batches used for voting
+        self.test_ratio = 1 - self.train_ratio
