cc7cc43fcbb18e0fc3a55e8ce11b3271037e9f2f
Emma Smith <emma.smith@fetch.ai>
2020-12-29 18:05:42 +0000

    Improved tests

--- test_new_keraslearner.py
+++ test_new_keraslearner.py
@@ -1,13 +1,15 @@
 from unittest import mock
 
+import pytest
+
 from colearn.ml_interface import Weights
 from new_keraslearner import NewKerasLearner
 
 
 def get_mock_model() -> mock.Mock:
     model = mock.Mock()
-    model.evaluate.return_value = {"loss": -42,
-                                   "accuracy": 100}
+    model.evaluate.return_value = {"loss": 1,
+                                   "accuracy": 3}
     model.get_weights.return_value = "all the weights"
     return model
 
@@ -16,26 +18,44 @@ def get_mock_dataloader() -> mock.Mock:
     return mock.Mock()
 
 
-def test_stuff():
+@pytest.fixture
+def nkl():
+    """Returns a NewKeraslearner"""
     model = get_mock_model()
     dl = get_mock_dataloader()
     nkl = NewKerasLearner(model, dl)
+    return nkl
 
-    assert nkl.vote_score == model.evaluate.return_value["loss"]
 
-    assert nkl.vote(0) is False
-    assert nkl.vote(-100) is True
+def test_vote(nkl):
+    assert nkl.vote_score == get_mock_model().evaluate.return_value["loss"]
 
+    assert nkl.vote(1.1) is False
+    assert nkl.vote(1) is True
+    assert nkl.vote(0.9) is True
+
+
+def test_minimise_criterion(nkl):
     nkl.minimise_criterion = False
 
-    assert nkl.vote(0) is True
-    assert nkl.vote(-100) is False
+    assert nkl.vote(1.1) is True
+    assert nkl.vote(1) is True
+    assert nkl.vote(0.9) is False
 
+
+def test_criterion(nkl):
     nkl.criterion = "accuracy"
+    nkl.mli_accept_weights(Weights(weights="foo"))
+    assert nkl.vote_score == get_mock_model().evaluate.return_value["accuracy"]
 
-    assert nkl.vote(200) is True
-    assert nkl.vote(-100) is False
 
+def test_propose_weights(nkl):
     weights = nkl.mli_propose_weights()
     assert type(weights) == Weights
-    assert weights.weights == model.get_weights.return_value
+    assert weights.weights == get_mock_model().get_weights.return_value
+
+
+def test_get_current_weights(nkl):
+    weights = nkl.mli_get_current_weights()
+    assert type(weights) == Weights
+    assert weights.weights == get_mock_model().get_weights.return_value
