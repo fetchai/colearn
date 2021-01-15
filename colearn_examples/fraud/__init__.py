3e787b9f68fe9dd0c17e691704a3859dc3a30f02
Emma Smith <emma.smith@fetch.ai>
2021-01-15 18:17:55 +0000

    Deleted old datasets

--- colearn_examples/fraud/__init__.py
+++
@@ -1,8 +0,0 @@
-from colearn_examples.utils.plot import display_statistics, plot_results, plot_votes
-
-from .data import split_to_folders, prepare_single_client
-
-from .config import FraudConfig
-
-__all__ = ["FraudConfig", "display_statistics", "plot_results", "plot_votes",
-           "split_to_folders", "prepare_single_client"]
