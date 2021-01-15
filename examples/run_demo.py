0a08c97087788e1e57943bd1ca1cac0b84b9d033
Emma Smith <emma.smith@fetch.ai>
2021-01-15 18:09:01 +0000

    Rename run_demo

--- examples/run_demo.py
+++ examples/run_demo.py
@@ -2,42 +2,53 @@
 import argparse
 import os
 
-from colearn_examples.config import TrainingData, ColearnConfig, TrainingMode
-from colearn_examples.training import main
+from colearn_examples_new.new_demo import main, TaskType
 
 parser = argparse.ArgumentParser(description='Run colearn demo')
-parser.add_argument("-d", "--data_dir", help="Directory for training data")
-parser.add_argument("-t", "--task", default="XRAY",
+parser.add_argument("-d", "--train_dir", default=None, help="Directory for training data")
+parser.add_argument("-e", "--test_dir", default=None, help="Directory for test data")
+
+parser.add_argument("-t", "--task", default="KERAS_MNIST",
                     help="Options are " + " ".join(str(x.name)
-                                                   for x in TrainingData))
+                                                   for x in TaskType))
+
+parser.add_argument("-m", "--model_type", default=None, type=str)
+
 parser.add_argument("-n", "--n_learners", default=5, type=int)
-parser.add_argument("-e", "--epochs", default=15, type=int)
+parser.add_argument("-p", "--n_epochs", default=15, type=int)
+
+parser.add_argument("-v", "--vote_threshold", default=0.5, type=float)
+
+parser.add_argument("-r", "--train_ratio", default=0.8, type=float)
+
 parser.add_argument("-s", "--seed", type=int, default=None)
-parser.add_argument("-m", "--mode", default="COLLECTIVE",
-                    help="Options are " + " ".join(str(x.name)
-                                                   for x in TrainingMode)
-                    )
+parser.add_argument("-l", "--learning_rate", type=float, default=None)
+parser.add_argument("-b", "--batch_size", type=int, default=None)
+
 args = parser.parse_args()
 
-# check data dir
-if args.task == "MNIST" or args.task == "CIFAR10":  # these data are downloaded
-    assert args.data_dir is None, \
-        "MNIST and CIFAR10 datasets are downloaded so " \
-        "data_dir should not be given"
-    args.data_dir = ""
-else:
-    data_dir = os.path.abspath(args.data_dir)
-    if not os.path.isdir(data_dir):
-        raise Exception("Data dir is not a directory: " + str(data_dir))
+# Generate seed
+if args.seed is None:
+    args.seed = int.from_bytes(os.urandom(4), byteorder="big")
 
-try:
-    task = TrainingData[args.task]
-except KeyError:
-    raise Exception("task %s not part of the TrainingData enum" % args.task)
+# Print seed for logs
+print("Seed is ", args.seed)
 
-config = ColearnConfig(task=task,
-                       n_learners=args.n_learners,
-                       n_epochs=args.epochs, seed=args.seed)
+# Optional arguments - will be replaced by default values depending on task/model if not set
+optional_learning_kwargs = dict()
+if args.learning_rate is not None:
+    optional_learning_kwargs["learning_rate"] = args.learning_rate
+if args.batch_size is not None:
+    optional_learning_kwargs["batch_size"] = args.batch_size
 
-config.mode = TrainingMode[args.mode]
-main(config, args.data_dir)
+main(str_task_type=args.task,
+     train_data_folder=args.train_dir,
+     test_data_folder=args.test_dir,
+     n_learners=args.n_learners,
+     n_epochs=args.n_epochs,
+     str_model_type=args.model_type,
+     vote_threshold=args.vote_threshold,
+     seed=args.seed,
+     shuffle_seed=args.seed,
+     train_ratio=args.train_ratio,
+     **optional_learning_kwargs)
