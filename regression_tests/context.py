cbb5c9e5e3b05f39cec9d5b1f6c2f4e169ec7cfd
Emma Smith <emma.smith@fetch.ai>
2021-01-15 18:58:00 +0000

    Removed regression tests for old code

--- regression_tests/context.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'colearn')))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', "colearn_examples")))
-
-from colearn_examples.config import ColearnConfig
-from colearn_examples.config import TrainingData
