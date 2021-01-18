62af7360cf0b772151cbe08457a4c23e5cfa99de
Emma Smith <emma.smith@fetch.ai>
2021-01-18 11:16:39 +0000

    Removed unused functions from utils/data

--- colearn/utils/data.py
+++ colearn/utils/data.py
@@ -1,8 +1,5 @@
 import random
 
-import cv2
-import numpy as np
-
 
 def shuffle_data(data_lists, seed=None):
     for i in range(len(data_lists) - 1):
@@ -26,33 +23,12 @@ def shuffle_data(data_lists, seed=None):
     return shuffled_lists
 
 
-def to_rgb_normalize_and_resize(img, width, height):
-    img = cv2.imread(str(img))
-    img = cv2.resize(img, (width, height))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = img.astype(np.float32) / 255.0
-    img = np.reshape(img, (width, height, 1))
-
-    return img
-
-
-def normalize_image(filename, class_num, width, height):
-    data = to_rgb_normalize_and_resize(filename, width, height)
-    label = class_num
-
-    return data, label
-
-
-def init_list_of_objects(size):
-    return [list() for _ in range(size)]
-
-
 # Splits data to len(chunks) parts and each chunks[i] defines percentage of
 # len(data)
 def split_by_chunksizes(data_lists, chunks):
     data_len = len(data_lists[0])
 
-    splitted_lists = init_list_of_objects(len(data_lists))
+    splitted_lists = [list() for _ in range(len(data_lists))]
 
     it = 0
     for chunk in chunks:
