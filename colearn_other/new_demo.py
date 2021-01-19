05c805a5166451634ec8e53cd8e596b365cd7ed1
Jiri Vestfal <45360788+MissingNO57@users.noreply.github.com>
2021-01-19 14:31:55 +0100

    MLI dataset factory (#79)
    Added mli_factory

--- colearn_other/new_demo.py
+++ colearn_other/new_demo.py
@@ -1,58 +1,44 @@
-from enum import Enum
+from typing import Optional
 
 from colearn.training import initial_result, collective_learning_round, set_equal_weights
 from colearn.utils.plot import plot_results, plot_votes
 from colearn.utils.results import Results
-
-
-class TaskType(Enum):
-    PYTORCH_XRAY = 1
-    KERAS_MNIST = 2
-    KERAS_CIFAR10 = 3
-    PYTORCH_COVID_XRAY = 4
-    FRAUD = 5
+from colearn_other.mli_factory import TaskType, mli_factory
 
 
 def main(str_task_type: str,
-         n_learners=5,
-         n_epochs=20,
-         vote_threshold=0.5,
-         train_data_folder: str = None,
-         test_data_folder: str = None,
-         str_model_type: str = None,
+         n_learners: int = 5,
+         n_epochs: int = 20,
+         vote_threshold: float = 0.5,
+         train_data_folder: Optional[str] = None,
+         test_data_folder: Optional[str] = None,
+         str_model_type: Optional[str] = None,
          **learning_kwargs):
     # Resolve task type
     task_type = TaskType[str_task_type]
 
-    # Load task
+    # Load correct split to folders function
     # pylint: disable=C0415
     if task_type == TaskType.PYTORCH_XRAY:
-        from colearn_pytorch.pytorch_xray import split_to_folders, prepare_learner, prepare_data_loaders, ModelType
+        from colearn_pytorch.pytorch_xray import split_to_folders, ModelType
     elif task_type == TaskType.KERAS_MNIST:
         # noinspection PyUnresolvedReferences
-        from colearn_keras.keras_mnist import (  # type: ignore[no-redef]
-            split_to_folders, prepare_learner, prepare_data_loaders, ModelType)
+        from colearn_keras.keras_mnist import split_to_folders, ModelType  # type: ignore[no-redef]
     elif task_type == TaskType.KERAS_CIFAR10:
         # noinspection PyUnresolvedReferences
-        from colearn_keras.keras_cifar10 import (  # type: ignore[no-redef]
-            split_to_folders, prepare_learner, prepare_data_loaders, ModelType)
+        from colearn_keras.keras_cifar10 import split_to_folders, ModelType  # type: ignore[no-redef]
     elif task_type == TaskType.PYTORCH_COVID_XRAY:
         # noinspection PyUnresolvedReferences
-        from colearn_pytorch.pytorch_covid_xray import (  # type: ignore[no-redef]
-            split_to_folders, prepare_learner, prepare_data_loaders, ModelType)
+        from colearn_pytorch.pytorch_covid_xray import split_to_folders, ModelType  # type: ignore[no-redef]
     elif task_type == TaskType.FRAUD:
         # noinspection PyUnresolvedReferences
-        from colearn_other.fraud_dataset import (  # type: ignore [no-redef]
-            split_to_folders, prepare_learner, prepare_data_loaders, ModelType)
+        from colearn_other.fraud_dataset import split_to_folders, ModelType  # type: ignore [no-redef]
     else:
         raise Exception("Task %s not part of the TaskType enum" % type)
 
-    # Resolve model type
-    if str_model_type is not None:
-        model_type = ModelType[str_model_type]
-    else:
-        # Get first model if not specified
-        model_type = list(ModelType)[0]
+    # Replace with default model type if not specified
+    if str_model_type is None:
+        str_model_type = ModelType(1).name
 
     # lOAD DATA
     train_data_folders = split_to_folders(
@@ -73,13 +59,11 @@ def main(str_task_type: str,
 
     all_learner_models = []
     for i in range(n_learners):
-        learner_dataloaders = prepare_data_loaders(train_folder=train_data_folders[i],
-                                                   test_folder=test_data_folders[i],
-                                                   **learning_kwargs)
-
-        all_learner_models.append(prepare_learner(model_type=model_type,
-                                                  data_loaders=learner_dataloaders,
-                                                  **learning_kwargs))
+        all_learner_models.append(mli_factory(str_task_type=str_task_type,
+                                              str_model_type=str_model_type,
+                                              train_folder=train_data_folders[i],
+                                              test_folder=test_data_folders[i]
+                                              ))
 
     set_equal_weights(all_learner_models)
     score_name = all_learner_models[0].score_name
