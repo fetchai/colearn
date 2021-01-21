1723825f92e2bb0d4ec3f81f6a9f0c8940c9dc33
Emma Smith <emma.smith@fetch.ai>
2021-01-21 17:13:24 +0000

    I misunderstood version specs

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 import setuptools
 
-keras_deps = ['tensorflow~=2.3']
-pytorch_deps = ['torch~=1.7']
+keras_deps = ['tensorflow~=2.3.0']
+pytorch_deps = ['torch~=1.7.0']
 docs_deps = ["mkdocs",
              "mkdocs-macros-plugin",
              "mkdocs-material",
              "mkdocs-material-extensions",
              "markdown-include"]
-examples_deps = ['opacus~=0.10',
-                 'opencv-python~=4.4',
-                 'pandas~=1.1',
-                 'scikit-learn~=0.23',
-                 'scipy~=1.5',
-                 'tensorflow_datasets~=4.2',
-                 'tensorflow-privacy~=0.5',
-                 'torchsummary~=1.5',
-                 'torchvision~=0.8']
+examples_deps = ['opacus~=0.10.0',
+                 'opencv-python~=4.4.0',
+                 'pandas~=1.1.0',
+                 'scikit-learn~=0.23.0',
+                 'scipy~=1.5.0',
+                 'tensorflow_datasets~=4.2.0',
+                 'tensorflow-privacy~=0.5.0',
+                 'torchsummary~=1.5.0',
+                 'torchvision~=0.8.0']
 
 all_deps = keras_deps + pytorch_deps + examples_deps
 
@@ -37,11 +37,11 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='~=3.7',
-    install_requires=['matplotlib~=3.3',
-                      'pydantic~=1.7',
-                      'numpy~=1.16'
+    install_requires=['matplotlib~=3.3.0',
+                      'pydantic~=1.7.0',
+                      'numpy~=1.16.0'
                       ],
-    tests_require=["tox~=3.20"],
+    tests_require=["tox~=3.20.0"],
     extras_require={
         'keras': keras_deps,
         'pytorch': pytorch_deps,
