5b7f898ec7f690e9250c6123906e9c9de76ab722
Emma Smith <emma.smith@fetch.ai>
2021-01-26 09:59:03 +0000

    Refactor cifar and mnist to use tfds datasets

--- colearn/utils/data.py
+++ colearn/utils/data.py
@@ -1,4 +1,6 @@
 import random
+from typing import List, Union
+import numpy as np
 
 
 def shuffle_data(data_lists, seed=None):
@@ -48,3 +50,23 @@ def split_by_chunksizes(data_lists, chunks):
         it += step
 
     return splitted_lists
+
+
+def split_list_into_fractions(input_list: Union[List, np.ndarray],
+                              fractions_list: List,
+                              min_part_size=1):
+    split_list = []
+    start_index = 0
+    n_indices = len(input_list)
+    for frac in fractions_list:
+        end_index = start_index + int(n_indices * frac)
+        if end_index >= n_indices:
+            end_index = n_indices
+
+        if end_index - start_index < min_part_size:
+            raise Exception("Insufficient data in this part")
+
+        split_list.append(input_list[start_index: end_index])
+        start_index = end_index
+
+    return split_list
