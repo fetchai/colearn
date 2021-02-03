06a14dfbfa8880609974a0e17f46c4a525f5926b
Juan Besa <juan.besa@fetch.ai>
2021-02-03 12:31:28 +0000

    chore: cleanup of example_mli_factory

--- colearn_grpc/example_mli_factory.py
+++ colearn_grpc/example_mli_factory.py
@@ -3,20 +3,19 @@ from inspect import signature
 from typing import Set, Dict, Any
 
 from colearn.ml_interface import MachineLearningInterface
-from colearn_other.mli_factory import TaskType, mli_factory
 from colearn_grpc.mli_factory_interface import MliFactory
+from colearn_keras.keras_mnist import prepare_data_loaders, ModelType
+from colearn_other.mli_factory import TaskType, mli_factory
 
 
 # TODO Add Documentation
-# TODO Add tests
 class ExampleMliFactory(MliFactory):
 
     def __init__(self):
         self.models = {task.name: {} for task in TaskType}
         self.dataloaders = {task.name: {} for task in TaskType}
 
-        from colearn_keras.keras_mnist import prepare_data_loaders, ModelType
-
+        # TODO Currently only KERAS_MNIST(2DConv) is supported
         self.models[TaskType.KERAS_MNIST.name] = {"model_type": ModelType(1).name}
         self.dataloaders[TaskType.KERAS_MNIST.name] = \
             {param.name: param.default
@@ -49,10 +48,14 @@ class ExampleMliFactory(MliFactory):
         data_config = self.dataloaders[dataloader_name]  # Default parameters
         data_config.update(json.loads(dataset_params))
 
+        # TODO Names should match between colearn and contract_learn
         train_folder = data_config["location"]
 
         model_config = self.models[model_name]  # Default parameters
         model_config.update(json.loads(model_params))
+        # model_type will allow you to choose different architectures for different tasks
+        # eventually we will get rid of it, but for now only the first model_type is supported
+        # and the name of the architecture is just the task
         model_type = model_config["model_type"]
         model_config.pop('model_type', None)  # Required because model_type is passed as argument as well
 
