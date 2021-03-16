fa20b15816d7126c76587e23eca56414c241c45e
Emma Smith <emma.smith@fetch.ai>
2021-03-16 10:35:21 +0000

    Commit to poke github

--- examples/xgb_reg_boston.py
+++ examples/xgb_reg_boston.py
@@ -160,7 +160,6 @@ for round_index in range(n_rounds):
     )
     print_results(results)
 
-    # then make an updating graph
     print_results(results)
     plot.plot_results_and_votes(results)
 
