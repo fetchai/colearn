e962518880d2ae764ddff261ff2561ee046189d8
Balint Pataki <patbaa@gmail.com>
2022-01-24 09:19:00 +0000

    resolve conflict

--- colearn_keras/keras_learner.py
+++ colearn_keras/keras_learner.py
@@ -25,7 +25,7 @@ except ImportError:
                     "add-ons please install colearn with `pip install colearn[keras]`.")
 from tensorflow import keras
 
-from colearn.ml_interface import MachineLearningInterface, Weights, ProposedWeights
+from colearn.ml_interface import MachineLearningInterface, Weights, ProposedWeights, ColearnModel, ModelFormat, convert_model_to_onnx
 from colearn.ml_interface import DiffPrivBudget, DiffPrivConfig, TrainingSummary
 from tensorflow_privacy.privacy.analysis.compute_dp_sgd_privacy import compute_dp_sgd_privacy
 from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras import make_keras_optimizer_class
