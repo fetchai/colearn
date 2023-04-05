9eb97101a789a2cf5e99939f079d25d328dfa43b
Hanna Wagner <hanna.wagner@7lytix.com>
2023-04-05 13:03:40 +0200

    Bug-Fix: path update.

--- colearn_grpc/test_grpc_server.py
+++ colearn_grpc/test_grpc_server.py
@@ -17,6 +17,8 @@
 # ------------------------------------------------------------------------------
 import json
 import time
+import os
+from pathlib import Path
 from colearn.ml_interface import PredictionRequest
 from colearn_grpc.example_mli_factory import ExampleMliFactory
 from colearn_grpc.grpc_server import GRPCServer
@@ -82,7 +84,13 @@ def test_grpc_server_with_example_grpc_learner_client():
 
     pred_name = "prediction_1"
 
-    location = "../tests/test_data/img_0.jpg"
+    GITHUB_ACTION = bool(os.getenv("GITHUB_ACTION", ""))
+    if GITHUB_ACTION:
+        COLEARN_DATA_DIR = Path("/pvc-data/")
+        location = str(COLEARN_DATA_DIR / "tests/test_data/img_0.jpg")
+    else:
+        # change this to your local directory
+        location = "../tests/test_data/img_0.jpg"
     # Overwrite specified data loader
     prediction = client.mli_make_prediction(
         PredictionRequest(name=pred_name, input_data=bytes(location, 'utf-8'),
